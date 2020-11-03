44ec39c5d89bdf0f8c4b89ca1d337813d1268ca8
Clarence <diyobe2207@gmail.com>
2020-11-03 21:38:18 +0100

    Added Blastzone + Blast fx + respawn + 2 Player controls

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> characters;
+    [SerializeField] private GameObject blastExplosion;
+    [SerializeField] private Transform respawnPosition;
+
+    public static GameManager Instance;
+
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+
+    public void BlastCharacter(GameObject blastedCharacter)
+    {
+        foreach(GameObject character in characters)
+        {
+            if (character == blastedCharacter)
+            {
+                Instantiate(blastExplosion, character.transform.position, Quaternion.identity);
+                character.SetActive(false);
+                RespawnCharacter(character);
+            }
+        }
+    }
+
+    private void RespawnCharacter(GameObject character)
+    {
+        character.SetActive(true);
+        character.transform.position = respawnPosition.position;
+    }
+}
