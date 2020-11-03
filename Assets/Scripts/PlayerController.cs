b5890913209c5bb09b4ba7dd8d667cfe448a7d97
Nam <nam.mirbeau@gmail.com>
2020-11-03 14:36:32 +0100

    Base des attaques

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public enum CharacterState
     Idle,
     Acting,
     Hit,
+    Ejected,
     Dead
 }
 
@@ -19,8 +20,19 @@ public class PlayerController : MonoBehaviour
 
 
     [SerializeField]
-    CharacterCollision characterCollision;
+    private Transform particlePoint; // Utilisé pour que les particles s'affichent bien au centre
+    public Transform ParticlePoint
+    {
+        get { return particlePoint; }
+    }
 
+    [SerializeField]
+    CharacterCollision characterCollision;
+    [SerializeField]
+    Animator characterAnimator;
+
+
+    [Space]
     [Header("Movement")]
     [SerializeField]
     float speedMax = 5;
@@ -35,31 +47,100 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     float airStop = 0.9f;
 
+    [Space]
+    [SerializeField]
+    protected float gravityForce = 8;
+    [SerializeField]
+    protected float gravityForceMax = -10;
+
+
+    [Space]
     [Header("Jump")]
     [SerializeField]
     float jumpImpulsion;
     [SerializeField]
     int numberOfJumps = 2;
 
-    [Header("Debug")]
-    public int direction;
-    public float currentSpeed;
+
+    [Space]
+    [Header("Knockback")]
+    [SerializeField]
+    float knockbackPowerReduce;
+    [SerializeField]
+    float knockbackPowerForWallBounce;
+    [SerializeField]
+    float knockbackWallBounce;
+
+
+
+    protected Vector2 knockbackPower;
+
+    CharacterState state;
+
+    private int direction;
+    public int Direction
+    {
+        get { return direction; }
+    }
+
+    float currentSpeedX;
+
     int currentNumberOfJumps;
 
+    bool endAction = false;
+    bool canEndAction = false;
 
+    AttackController currentAttack;
+    AttackController currentAttackController;
+
+    IEnumerator motionSpeedCoroutine;
+
+
+    List<input> buffer;
 
     private void Start()
     {
         characterCollision.doAction += ResetJump;
+        characterCollision.OnWallCollision += WallBounce;
+        characterCollision.OnGroundCollision += GroundBounce;
+    }
+    public void UpdateBuffer(List<input> inputBuffer)
+    {
+        buffer = inputBuffer;
+    }
+
+    private void Update()
+
+    {   // OnTriggerEnter 
+        // OnColliderEnter
+        // Update
+        // Cette ligne est pour empêcher qu'il y ait un bug d'animation au moment où le perso joue une action pile à la frame ou le perso termine son action précédente
+        if (canEndAction == false)
+            canEndAction = true;
+
+        // Input
+        if (state == CharacterState.Hit)
+        {
+            UpdateKnockback();
+        }
+        else
+        {
+            UpdateControls();
+            ApplyGravity();
+        }
+
+        // Cette ligne est pour empêcher qu'il y ait un bug d'animation au moment où le perso joue une action pile à la frame ou le perso termine son action précédente
+        if (endAction == true)
+            EndAction();
     }
 
     private void ResetJump()
     {
         currentNumberOfJumps = numberOfJumps;
-        direction = (int)Mathf.Sign(currentSpeed);
+        direction = (int)Mathf.Sign(currentSpeedX);
     }
 
-    public void UpdateBuffer(List<input> buffer)
+    void UpdateControls()
     {
         CheckJump(buffer);
         CheckHorizontal(buffer);
@@ -84,31 +165,217 @@ public class PlayerController : MonoBehaviour
         {
             if (buffer[0].horizontal != 0)
             {
-                currentSpeed += buffer[0].horizontal * acceleration;
-                direction = (int) Mathf.Sign(currentSpeed);
+                currentSpeedX += buffer[0].horizontal * acceleration;
+                direction = (int) Mathf.Sign(currentSpeedX);
             }
             else
             {
-                currentSpeed -= (decceleration * direction);
-                if (currentSpeed <= decceleration && currentSpeed >= -decceleration)
-                    currentSpeed = 0;
+                currentSpeedX -= (decceleration * direction);
+                if (currentSpeedX <= decceleration && currentSpeedX >= -decceleration)
+                    currentSpeedX = 0;
             }
         }
         else
         {
             if (buffer[0].horizontal != 0)
             {
-                currentSpeed += buffer[0].horizontal * (acceleration - airFriction);
+                currentSpeedX += buffer[0].horizontal * (acceleration - airFriction);
             }
             else
             {
-                currentSpeed *= airStop;
-                if (currentSpeed <= airFriction && currentSpeed >= -airFriction)
-                    currentSpeed = 0;
+                currentSpeedX *= airStop;
+                if (currentSpeedX <= airFriction && currentSpeedX >= -airFriction)
+                    currentSpeedX = 0;
             }
         }
-        currentSpeed = Mathf.Clamp(currentSpeed, -speedMax, speedMax);
-        characterCollision.Move(currentSpeed);
+        currentSpeedX = Mathf.Clamp(currentSpeedX, -speedMax, speedMax);
+        characterCollision.MoveX(currentSpeedX);
 
     }
+
+
+
+
+
+
+
+
+
+
+    private void ApplyGravity()
+    {
+        if (characterCollision.IsGrounded == true)
+        {
+            //currentSpeedY = 0;
+            return;
+        }
+        characterCollision.ApplyGravity(gravityForce, gravityForceMax);
+    }
+
+
+
+
+    // ==========================================================================================================
+    //    A C T I O N
+    // ==========================================================================================================
+
+
+    public void Action(AttackController action)
+    {
+        if (currentAttackController != null)
+            currentAttackController.ActionEnd();
+        currentAttackController = null;
+
+        currentAttack = action;
+        characterAnimator.ResetTrigger("Idle");
+        characterAnimator.Play(currentAttack.AttackAnimation.name, 0, 0f);
+
+        endAction = false;
+        canEndAction = false;
+    }
+
+
+    // Appelé par les anims
+    // Uniquement utilisable pour l'action principale
+    public void ActionActive()
+    {
+        if (currentAttack != null)
+        {
+            currentAttackController = (Instantiate(currentAttack, this.transform.position, Quaternion.identity));
+            currentAttackController.CreateAttack(this);
+        }
+    }
+
+
+    // Appelé par les anims
+    // Uniquement utilisable pour dire d'arrêter l'action
+    public void CanEndAction()
+    {
+        if(canEndAction == true)
+        {
+            endAction = true;
+        }
+    }
+
+    private void EndAction()
+    {
+        if (currentAttackController != null)
+            currentAttackController.ActionEnd();
+    }
+
+
+
+    // ==========================================================================================================
+    //    A N I M A T I O N
+    // ==========================================================================================================
+
+    public void SetCharacterMotionSpeed(float newSpeed, float time = 0)
+    {
+        characterCollision.CharacterMotionSpeed = newSpeed;
+        characterAnimator.speed = newSpeed;
+        /*if (currentAttackController != null)
+            currentAttackController.AttackMotionSpeed(newSpeed);*/
+        if (time > 0)
+        {
+            if (motionSpeedCoroutine != null)
+                StopCoroutine(motionSpeedCoroutine);
+            motionSpeedCoroutine = MotionSpeedCoroutine(time);
+            StartCoroutine(motionSpeedCoroutine);
+        }
+    }
+
+
+    private IEnumerator MotionSpeedCoroutine(float time)
+    {
+        while (time > 0)
+        {
+            time -= Time.deltaTime;
+            yield return null;
+        }
+        characterCollision.CharacterMotionSpeed = 1;
+        characterAnimator.speed = 1;
+        /*if (currentAttackController != null)
+            currentAttackController.AttackMotionSpeed(characterMotionSpeed);*/
+    }
+
+
+    public float GetMotionSpeed()
+    {
+        return characterAnimator.speed;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // ==========================================================================================================
+    //    K N O C K B A C K
+    // ==========================================================================================================
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Attaque ennemi détecté
+        if(other.tag != this.transform.tag)
+        {
+            Knockback(other.GetComponent<AttackController>());
+        }
+
+    }
+
+    private void Knockback(AttackController attack)
+    {
+
+        state = CharacterState.Hit;
+        currentSpeedX = 0;
+        Vector2 direction = this.transform.position - attack.transform.position;
+        direction *= attack.KnockbackPower;
+        knockbackPower = direction;
+
+        attack.HasHit(this);
+
+    }
+
+    protected void UpdateKnockback()
+    {
+        if (GetMotionSpeed() == 0)
+            return;
+        characterCollision.Move(knockbackPower.x, knockbackPower.y);
+        float reduce = knockbackPowerReduce * Time.deltaTime * GetMotionSpeed();
+        knockbackPower -= new Vector2(reduce * Mathf.Sign(knockbackPower.x), reduce * Mathf.Sign(knockbackPower.y));
+        if(knockbackPower.magnitude < knockbackPowerForWallBounce )
+        {
+            state = CharacterState.Idle;
+        }
+    }
+
+
+
+
+
+    public void WallBounce(Transform collider)
+    {
+        if(state == CharacterState.Hit)
+        {
+            knockbackPower.x = -knockbackPower.x;
+            //SetCharacterMotionSpeed(0, 10);
+        }
+    }
+
+    public void GroundBounce(Transform collider)
+    {
+        if (state == CharacterState.Hit)
+        {
+            knockbackPower.y = -knockbackPower.y;
+            //SetCharacterMotionSpeed(0, 10);
+        }
+    }
+
 }
