8a55b25727fa15bbb97cfda5004bd94f89c0e656
Nam <nam.mirbeau@gmail.com>
2020-11-06 00:16:06 +0100

    Final Destination

---
+++ Assets/Scripts/Menu/Tempest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tempest : MonoBehaviour
+{
+    [SerializeField]
+    Material newSkybox;
+    public void EventTempest()
+    {
+        RenderSettings.skybox = newSkybox;
+    }
+}
