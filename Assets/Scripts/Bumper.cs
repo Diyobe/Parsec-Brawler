2a8b97f0ba979f31728ca61364bc15af60a96dba
Nam <nam.mirbeau@gmail.com>
2020-11-05 15:43:16 +0100

    Bumper fix

--- Assets/Scripts/Bumper.cs
+++ Assets/Scripts/Bumper.cs
@@ -12,9 +12,10 @@ public class Bumper : MonoBehaviour
     {
         if (other.gameObject.tag == "Player1" || other.gameObject.tag == "Player2" || other.gameObject.tag == "Player3" || other.gameObject.tag == "Player4")
         {
-            Vector2 direction = other.transform.position - this.transform.position;
-            direction *= bumperForce;
+
             PlayerController player = other.GetComponent<PlayerController>();
+            Vector2 direction = player.ParticlePoint.position - this.transform.position;
+            direction *= bumperForce;
             player.AddForce(direction.x, direction.y);
         }
     }
