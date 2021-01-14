cd6a69c373dc05f112e5b97f8bb9424c282c0783
Nam <nam.mirbeau@gmail.com>
2021-01-14 13:45:16 +0100

    Asset stage superman

---
+++ Assets/Scripts/SupermanStageGravity.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SupermanStageGravity : MonoBehaviour
+{
+    [SerializeField]
+    float gravityModifier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
