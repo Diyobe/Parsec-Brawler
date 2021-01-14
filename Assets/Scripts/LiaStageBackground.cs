98fd2acdbf81c751e525e08ce54f12fa56437d56
Nam <nam.mirbeau@gmail.com>
2021-01-14 15:58:06 +0100

    Stage de Lia

---
+++ Assets/Scripts/LiaStageBackground.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LiaStageBackground : MonoBehaviour
+{
+    [SerializeField]
+    BattleManager battleManager;
+    [SerializeField]
+    BlastZone[] blastZones;
+
+    [SerializeField]
+    Animator cameraAnimation;
+
+    void Start()
+    {
+        StartCoroutine(InitializationCoroutine());
+    }
+
+    private IEnumerator InitializationCoroutine()
+    {
+        yield return new WaitForSeconds(1f);
+        for (int i = 0; i < battleManager.PlayersAlive.Count; i++)
+        {
+            battleManager.PlayersAlive[i].OnFlashMove += FlashMoveFeedback;
+            battleManager.PlayersAlive[i].OnSuperKnockback += SuperKnockbackFeedback;
+        }
+        for (int i = 0; i < blastZones.Length; i++)
+        {
+            blastZones[i].OnBlast += BlastZoneKill;
+        }
+    }
+
+    public void FlashMoveFeedback(PlayerController p)
+    {
+        cameraAnimation.SetTrigger("FeedbackFlashMove");
+    }
+    public void BlastZoneKill(PlayerController p)
+    {
+        cameraAnimation.SetTrigger("FeedbackBlast");
+    }
+    public void SuperKnockbackFeedback(PlayerController p)
+    {
+        cameraAnimation.SetTrigger("FeedbackSuperKnockback");
+    }
+}
